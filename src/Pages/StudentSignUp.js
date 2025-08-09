5efc6f30c06fa6d651c59daa130cf174f720243f
ZEESHAN SARFRAZ <zeeshan303.3.1@gmail.com>
2025-08-09 19:45:20 +0500

    Update StudentSignUp.js

--- src/Pages/StudentSignUp.js
+++ src/Pages/StudentSignUp.js
@@ -165,14 +165,23 @@ export default function SignupForm() {
                         <option value="2">B</option>
                     </select>
 
-                    <input
+                    <select
                         name="semester"
                         value={formData.semester}
                         onChange={handleChange}
-                        placeholder="Semester"
                         required
                         className={classes.input}
-                    />
+                    >
+                        <option value="">Select Semester</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                    </select>
 
                     {/* Registration Number with validation */}
                     <input
