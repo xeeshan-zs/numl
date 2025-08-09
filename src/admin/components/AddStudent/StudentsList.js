6245cccb06dacb904f0ba7d2c7d5cf39fc2b1211
ZEESHAN SARFRAZ <zeeshan303.3.1@gmail.com>
2025-08-09 20:09:25 +0500

    search button added on admin student view

--- src/admin/components/AddStudent/StudentsList.js
+++ src/admin/components/AddStudent/StudentsList.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
 import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '../../../firebaseConfig';
 
@@ -9,7 +8,6 @@ import AddFeeSummary from '../AddFeeSummary/AddFeeSummary';
 import AddPreviousAcademicRecord from '../AddPrevRecord/AddPrevRecord';
 
 import classes from './StudentList.module.css';
-
 import Loader from '../../../assets/loader.png';
 
 import { sortByName, sortByRegistrationNo, sortBySemester } from '../../utility/sortHelpers';
@@ -23,6 +21,7 @@ function StudentList() {
   const [loading, setLoading] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
   const [isRecording, setIsRecording] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   // Fetch student data from Firestore
   useEffect(() => {
@@ -35,7 +34,6 @@ function StudentList() {
         ...doc.data(),
       }));
       setStudents(studentsData);
-      console.log(studentsData[0]);
       setLoading(false);
     };
 
@@ -72,76 +70,100 @@ function StudentList() {
     setIsUpdating(false);
     setShowUpdateModal(false);
   };
+
   const handleRecordModal = () => {
     setIsRecording(false);
     setShowUpdateModal(false);
   };
 
+  // Filtered students list
+  const filteredStudents = students.filter(student =>
+      student.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      student.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      student.stdRegNumber.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   const TableRow = ({ student, index }) => (
-    <tr key={student.username}>
-      <td>{index + 1}</td>
-      <td onClick={() => handleView(student)}>{`${student.firstName} ${student.lastName}`}</td>
-      <td onClick={() => handleView(student)}>{student.stdRegNumber}</td>
-      <td>{student.semester}</td>
-      <td>{getProgramName(student.program)}</td>
-      <td className={classes.actions}>
-        <button onClick={() => handleView(student)} className={classes.viewButton}>
-          View
-        </button>
-        <button onClick={() => handleUpdate(student)} className={classes.updateButton}>
-          Update
-        </button>
-        <button onClick={() => handleDelete(student.id)} className={classes.deleteButton}>
-          Delete
-        </button>
-      </td>
-    </tr>
+      <tr key={student.id}>
+        <td>{index + 1}</td>
+        <td onClick={() => handleView(student)}>{`${student.firstName} ${student.lastName}`}</td>
+        <td onClick={() => handleView(student)}>{student.stdRegNumber}</td>
+        <td>{student.semester}</td>
+        <td>{getProgramName(student.program)}</td>
+        <td className={classes.actions}>
+          <button onClick={() => handleView(student)} className={classes.viewButton}>
+            View
+          </button>
+          <button onClick={() => handleUpdate(student)} className={classes.updateButton}>
+            Update
+          </button>
+          <button onClick={() => handleDelete(student.id)} className={classes.deleteButton}>
+            Delete
+          </button>
+        </td>
+      </tr>
   );
 
   return (
-    <div className={classes.studentListContainer}>
-      <h2 className={classes.title}>Student List</h2>
-      {loading && <img src={Loader} className={classes.loader} alt="loader-icon" />}
-      {!loading && students.length !== 0 && (
-        <table className={classes.studentTable}>
-          <thead>
-            <tr>
-              <th>Sr.No</th>
-              <th onClick={() => sortByName(students, setStudents)}>Name ⬆</th>
-              <th onClick={() => sortByRegistrationNo(students, setStudents)}>Registration No. ⬆</th>
-              <th onClick={() => sortBySemester(students, setStudents)}>Semester ⬆</th>
-              <th>Program</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map((student, index) => (
-              <TableRow student={student} index={index} uniqueness={student.id} />
-            ))}
-          </tbody>
-        </table>
-      )}
-      {students.length === 0 && !loading && <p>No records to show.</p>}
-      {showViewModal && selectedStudent && !isUpdating && !isRecording && (
-        <StudentViewModal student={selectedStudent} onClose={() => setShowViewModal(false)} />
-      )}
-      {showUpdateModal && selectedStudent && !isUpdating && !isRecording && (
-        <StudentUpdateModal
-          student={selectedStudent}
-          onClose={() => setShowUpdateModal(false)}
-          handleFeeSummary={setIsUpdating}
-          handlePrevRecord={setIsRecording}
-          onUpdate={updatedStudent => {
-            setStudents(students.map(student => (student.id === updatedStudent.id ? updatedStudent : student)));
-            setShowUpdateModal(false);
-          }}
+      <div className={classes.studentListContainer}>
+        <h2 className={classes.title}>Student List</h2>
+
+        {/* Search Box */}
+        <input
+            type="text"
+            className={classes.searchInput}
+            placeholder="Search by Name or Registration Number..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
         />
-      )}
-      {isUpdating && !isRecording && <AddFeeSummary studentId={selectedStudent.id} closeModal={handleFeeModal} />}
-      {isRecording && !isUpdating && (
-        <AddPreviousAcademicRecord studentId={selectedStudent.id} onClose={handleRecordModal} />
-      )}
-    </div>
+
+        {loading && <img src={Loader} className={classes.loader} alt="loader-icon" />}
+
+        {!loading && filteredStudents.length !== 0 && (
+            <table className={classes.studentTable}>
+              <thead>
+              <tr>
+                <th>Sr.No</th>
+                <th onClick={() => sortByName(students, setStudents)}>Name ⬆</th>
+                <th onClick={() => sortByRegistrationNo(students, setStudents)}>Registration No. ⬆</th>
+                <th onClick={() => sortBySemester(students, setStudents)}>Semester ⬆</th>
+                <th>Program</th>
+                <th>Actions</th>
+              </tr>
+              </thead>
+              <tbody>
+              {filteredStudents.map((student, index) => (
+                  <TableRow student={student} index={index} key={student.id} />
+              ))}
+              </tbody>
+            </table>
+        )}
+
+        {filteredStudents.length === 0 && !loading && <p>No records to show.</p>}
+
+        {showViewModal && selectedStudent && !isUpdating && !isRecording && (
+            <StudentViewModal student={selectedStudent} onClose={() => setShowViewModal(false)} />
+        )}
+
+        {showUpdateModal && selectedStudent && !isUpdating && !isRecording && (
+            <StudentUpdateModal
+                student={selectedStudent}
+                onClose={() => setShowUpdateModal(false)}
+                handleFeeSummary={setIsUpdating}
+                handlePrevRecord={setIsRecording}
+                onUpdate={updatedStudent => {
+                  setStudents(students.map(student => (student.id === updatedStudent.id ? updatedStudent : student)));
+                  setShowUpdateModal(false);
+                }}
+            />
+        )}
+
+        {isUpdating && !isRecording && <AddFeeSummary studentId={selectedStudent.id} closeModal={handleFeeModal} />}
+
+        {isRecording && !isUpdating && (
+            <AddPreviousAcademicRecord studentId={selectedStudent.id} onClose={handleRecordModal} />
+        )}
+      </div>
   );
 }
 
